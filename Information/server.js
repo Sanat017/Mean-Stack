dff78daefad10fa0d1adfdaa4292211bcef359fa
Sanat017 <sanatkabi1993@gmail.com>
2017-10-31 10:30:25 +0530

    Update server.js

--- Information/server.js
+++ Information/server.js
@@ -7,7 +7,6 @@ const port=3000;
 const app= express();
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.get('/informationlist',function(req,res){
     db.users.find(function(err,info){
